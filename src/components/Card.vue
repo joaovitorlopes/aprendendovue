469a75cdd9749628e77325f6f801e38dcc7018ef
João Vitor Lopes <jvlaaa@gmail.com>
2021-04-07 16:27:27 -0400

    Adicionando meu projeto ao git
    Estou adicionando ao github

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-col>
+            <b-card
+                title="Curso de VueJS"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Curso de Vue.JS"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2">
+                <b-card-text>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, laudantium eos id iure natus recusandae fugit eveniet sint provident rerum quis, autem alias velit suscipit ullam cum, deserunt consequuntur mollitia.
+                </b-card-text>
+                <b-button variant="primary">Inscrever-se</b-button>
+            </b-card>
+        </b-col>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container>
+    <b-row align-v="center">
+      <Card></Card>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import Card from '@/components/Card.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Card
+  }
+}
+</script>
